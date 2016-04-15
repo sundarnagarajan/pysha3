19f5ad368e3d5652488a88341cac04136b92724f
sundarnagarajan <sun.nagarajan@gmail.com>
2016-04-14 20:37:49 -0700

    Update setup.py

--- setup.py
+++ setup.py
@@ -11,11 +11,10 @@ _sha3 = Extension('_sha3',
                              'src/KeccakSponge.c',
                              'src/64/KeccakF-1600-opt64.c'])
 
-setup(name='_sha3',
+setup(name='sha3',
       version='0.2',
       description='SHA-3 implementation for Python',
-      author=u'Björn Edström',
-      author_email='be@bjrn.se',
-      url='https://github.com/bjornedstrom/python-sha3',
+      author=u'Sundar Nagarajan',
+      url='https://github.com/sundarnagarajan/pysha3',
       ext_modules=[_sha3],
       packages=['sha3'])
